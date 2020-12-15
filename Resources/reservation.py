0ee84889543fbe1563a8dc837c2b0446a081b7d7
Tino Tuomisto <tinotuomisto@live.com>
2020-12-15 03:49:16 +0200

    reservation models and resources
    Reservation models and resources

---
+++ Resources/reservation.py
@@ -0,0 +1,113 @@
+from flask import request
+from flask_restful import Resource
+from http import HTTPStatus
+
+from models.room import Room
+from models.reservation import Reservation
+
+from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
+from datetime import datetime
+
+
+class ReservationListResource(Resource):
+    def get(self):
+
+        reservations = Reservation.get_by_listed()
+        data = []
+
+        for reservation in reservations:
+            if not reservation.time < datetime.now():
+                data.append(reservation.data())
+
+        return {'data': data}, HTTPStatus.OK
+
+    @jwt_required
+    def post(self):
+
+        def is_valid_date(year, month, day):
+            day_count_for_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+            if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+                day_count_for_month[2] = 29
+            return 1 <= month <= 12 and 1 <= day <= day_count_for_month[month]
+
+        json_data = request.get_json()
+        current_user = get_jwt_identity()
+
+        if not Room.get_by_name(json_data['room']):
+            return {'message': 'Room does not exist'}, HTTPStatus.BAD_REQUEST
+
+        date_time_str = json_data['date']
+        date_time_obj = datetime.strptime(date_time_str, '%d/%m/%y')
+
+        if not 15 < json_data['time'] < 21:
+            return {'message': 'Only hours between 16-20 can be booked'}, HTTPStatus.BAD_REQUEST
+
+        if date_time_obj < datetime.now():
+            return {'message': 'Date already passed'}, HTTPStatus.BAD_REQUEST
+
+        if not is_valid_date(date_time_obj.year, date_time_obj.month, date_time_obj.day):
+            return {'not a real date'}, HTTPStatus.BAD_REQUEST
+
+        if date_time_obj.weekday() > 5:
+            return {'message': 'Room cant be reserved for the weekend'}, HTTPStatus.BAD_REQUEST
+
+        if Reservation.check_booked(date_time_obj, json_data['time'], json_data['room']):
+            return {'message': 'Already booked'}, HTTPStatus.BAD_REQUEST
+
+        reservation = Reservation(name=json_data['name'], user_id=current_user, room=json_data['room'],
+                                  description=json_data['description'], date=date_time_obj, time=json_data['time'])
+        reservation.save()
+        return reservation.data(), HTTPStatus.CREATED
+
+
+class ReservationResource(Resource):
+
+    @jwt_required
+    def put(self, reservation_id):
+
+        json_data = request.get_json()
+
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.room_id = json_data['room_id']
+        reservation.description = json_data['description']
+        reservation.time = json_data['time']
+        reservation.date = json_data['date']
+
+        reservation.save()
+
+        return reservation.data(), HTTPStatus.OK
+
+    @jwt_required
+    def delete(self, reservation_id):
+
+        reservation = Reservation.get_by_id(reservation_id=reservation_id)
+
+        if reservation is None:
+            return {'message': 'Reservation not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != reservation.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        reservation.delete()
+
+        return {}, HTTPStatus.NO_CONTENT
+
+    def get(self, room, date):
+        reservations = Reservation.get_by_date_room(date, room)
+        data = []
+
+        for reservation in reservations:
+            data.append(reservation.data())
+
+        return {'data': data}, HTTPStatus.OK
--- models/reservation.py
+++ models/reservation.py
@@ -4,28 +4,52 @@ from extensions import db
 #Että tää tuli näkyviin databaseen piti env.py tiedostoon piti lisätä "from models import reservation, room, user"
 
 class Reservation(db.Model):
-    
     __tablename__ = 'reservation'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
     description = db.Column(db.String(200))
-    listed = db.Column(db.Boolean(), default=False)
-    time = db.Column(db.DateTime(), nullable=False)
+    date = db.Column(db.Date(), nullable=False)
+    time = db.Column(db.Integer, nullabe=False)
+    is_listed = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
     updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
-    room_id = db.Column(db.Integer(), nullable=False)
+    room = db.Column(db.String(), nullable=False)
+
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
+    def data(self):
+        return {'id': self.id, 'name': self.name, 'description': self.description,
+                'date': self.date.strftime("%d/%m/%Y"), 'time': self.time,
+                'user': User.get_by_id(self.user_id).username, 'room': self.room}
 
     #roomreservation_id = db.Column(db.Integer(), db.ForeignKey("room.id"))
     #Linkataan reservatio > user settikeijo Tino tekee
 
-    def data(self):
-        return {'id': self.id, 'description': self.description, 'time': self.time, 'user_id': self.user_id,
-                'room_id': self.room_id}
-
 
     @classmethod
-    def get_by_id(cls, id):
-        return cls.query.filter_by(id=id).first()
+    def get_by_listed(cls):
+        return cls.query.filter_by(is_listed=True).all()
+
+    @classmethod
+    def get_by_date_time_room(cls, date, time, room):
+        return cls.query.filter_by(date=date, time=time, room=room).first()
+
+    @classmethod
+    def get_by_date_room(cls, date, room):
+        return cls.query.filter_by(date=date, room=room).all()
+
+    @classmethod
+    def get_by_date(cls, date):
+        return cls.query.filter_by(date=date).all()
+
+    @classmethod
+    def get_by_id(cls, reservation_id):
+        return cls.query.filter_by(id=reservation_id).first()
+
+    @classmethod
+    def get_by_room(cls, room):
+        return cls.query.filter_by(room=room).all()
 
     def save(self):
         db.session.add(self)
